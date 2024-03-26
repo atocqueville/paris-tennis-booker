1d74174524ede26cd4be2ef970ad330f29ca5644
Alexandre de Tocqueville <alex.detocqueville@gmail.com>
2024-03-27 00:06:34 +0100

    [Tech](Tooling) db scripts + seeding

--- backend/prisma/seed-bookings.ts
+++ backend/prisma/seed-bookings.ts
@@ -3,15 +3,36 @@ import { BookingStatus } from '../src/models/booking.model';
 
 const prisma = new PrismaClient();
 
-// create a new user
-await prisma.booking.create({
-    data: {
+const bookingsSample = [
+    {
         location: 'Roland Garros',
         startDate: new Date('2022-05-22'),
         status: BookingStatus.PENDING,
     },
-});
+    {
+        location: 'Suzanne Lenglen',
+        startDate: new Date('2022-06-15'),
+        status: BookingStatus.CONFIRMED,
+    },
+    {
+        location: 'Philippe Chatrier',
+        startDate: new Date('2022-07-10'),
+        status: BookingStatus.FAILED,
+    },
+];
 
-// count the number of users
-const count = await prisma.booking.count();
-console.log(`There are ${count} bookings in the database.`);
+async function main() {
+    await prisma.booking.create({ data: bookingsSample[0] });
+    await prisma.booking.create({ data: bookingsSample[1] });
+    await prisma.booking.create({ data: bookingsSample[2] });
+}
+
+await main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
